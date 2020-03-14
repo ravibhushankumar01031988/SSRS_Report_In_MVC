0af8c24130251e64f37ac80d8c9ae14ac79eb138
ravibhushankumar01031988 <61745107+ravibhushankumar01031988@users.noreply.github.com>
2020-03-14 23:24:09 +0530

    Add project files.

---
+++ SSRS_Report_In_MVC/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace SSRS_Report_In_MVC
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ SSRS_Report_In_MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SSRS_Report_In_MVC.MvcApplication" Language="C#" %>
